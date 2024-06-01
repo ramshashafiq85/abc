d2894226ea40f9759623c4fe175393ccf66b513b
ramshashafiq85 <155374529+ramshashafiq85@users.noreply.github.com>
2024-06-01 14:44:24 -0700

    my second commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let greet = "hello world!";
 console.log(greet);
+greet = "hello world from ramsha";
+console.log(greet);
 export {};
--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
 let greet="hello world!"
 console.log(greet)
+greet="hello world from ramsha"
+console .log(greet)
